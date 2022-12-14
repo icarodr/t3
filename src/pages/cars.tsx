e07d8d5062556382ab1b72d753b8d326aedd8806
icarodr <it46009@gmail.com>
2022-12-14 00:06:00 -0300

    some changes

--- src/pages/cars.tsx
+++ src/pages/cars.tsx
@@ -4,22 +4,27 @@ import { trpc } from "../utils/trpc";
 
 
 const CarPage: NextPage = () => {
+    
+  //Passo 1
     const response = trpc.car.getAll.useQuery();
     const { mutate } = trpc.car.create.useMutation({
         onSuccess: (response: any) => alert(JSON.stringify(response)),
     });
-
     const { mutate: deleteCar } = trpc.car.delete1.useMutation({
       onSuccess: (response) => alert(JSON.stringify(response))
-    })
+    });
+    
+    //Passo 2
     const [carName, setCarName] = useState("");
     const [carBrand, setCarBrand] = useState("");
     const [carPrice, setCarPrice] = useState("");
-
+    
+    //Passo 3
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
         mutate({ carName, carBrand, carPrice: Number(carPrice)});
     };
+
     return (
       <div className="px-12 pt-9">
       <h1 className="text-2xl font-bold">Cars: </h1>
